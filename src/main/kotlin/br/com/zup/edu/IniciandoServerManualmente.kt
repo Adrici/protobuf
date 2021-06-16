3773c2602c29c2f160bb950884a5abf246058499
Adrici <cirellia5@gmail.com>
2021-06-16 15:13:34 -0300

    IniciandoServerManualmente

---
+++ src/main/kotlin/br/com/zup/edu/IniciandoServerManualmente.kt
@@ -0,0 +1,49 @@
+package br.com.zup.edu
+
+import br.com.zup.FuncionarioRequest
+import br.com.zup.FuncionarioResponse
+import br.com.zup.FuncionarioServiceGrpc
+import com.google.protobuf.Timestamp
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+fun main() {
+
+    val server = ServerBuilder
+        .forPort(50051) //porta utilizada
+        .addService(FuncionarioEndpoint())
+        .build()
+
+    server.start()
+    server.awaitTermination()
+}
+
+class FuncionarioEndpoint : FuncionarioServiceGrpc.FuncionarioServiceImplBase() {
+    override fun cadastrar(request: FuncionarioRequest?, responseObserver: StreamObserver<FuncionarioResponse>?) {
+
+        println(request!!)
+
+        var nome: String? = request.nome
+
+        //verifica se um campo foi preenchido
+        if(!request.hasField(FuncionarioRequest.getDescriptor().findFieldByName("nome"))){
+            nome = "[Não preenchido]"
+        }
+
+        val instant = LocalDateTime.now().atZone(ZoneId.of("UTC")).toInstant()
+        val criadoEm = Timestamp.newBuilder()
+            .setSeconds(instant.epochSecond)
+            .setNanos(instant.nano)
+            .build()
+
+        val response = FuncionarioResponse.newBuilder()
+            .setNome(nome)
+            .setCriadoEm(criadoEm)
+            .build()
+
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
+    }
+}
